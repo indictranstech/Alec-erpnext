62a96387837ec5e1bc2b17956885adf0fbcf9d4d
Alec Ruiz-Ramon <alecruiz-ramon@ubuntu.ubuntu-domain>
2015-11-11 16:59:52 -0500

    communication history prompt, no save

--- erpnext/crm/doctype/prospect/prospect.js
+++ erpnext/crm/doctype/prospect/prospect.js
@@ -11,6 +11,8 @@ erpnext.ProspectController = frappe.ui.form.Controller.extend({
 		erpnext.toggle_naming_series();
 
 		if(!this.frm.doc.__islocal && this.frm.doc.__onload && !this.frm.doc.__onload.is_lead) {
+			this.frm.add_custom_button(__("Log Communication"), this.log_communication,
+				frappe.boot.doctype_icons["Communication"], "btn-big");
 			this.frm.add_custom_button(__("Convert to Lead"), this.create_lead,
 				frappe.boot.doctype_icons["Lead"], "btn-big");
 		}
@@ -21,6 +23,26 @@ erpnext.ProspectController = frappe.ui.form.Controller.extend({
 			method: "erpnext.crm.doctype.prospect.prospect.make_lead",
 			frm: cur_frm
 		})
+	},
+
+	log_communication: function(){
+		frappe.prompt([
+				{'default': get_today(),'fieldname': 'communication_date', 'fieldtype': 'Date', 'label': 'Date', 'reqd': 1},
+    			{'default': cur_frm.doc.prospect_owner, 'fieldname': 'sender','fieldtype': 'Link', 'label': 'Sender', 'options': 'User', 'reqd': 1},
+    			{'default': cur_frm.doc.prospect_name, 'fieldname': 'recipient', 'fieldtype': 'Link', 'label': 'Recipients', 'options': 'User', 'reqd': 1},
+    			{'fieldname': "cb01","fieldtype": "Column Break"},
+    			{'default': 'Sent', 'fieldname': 'sent_or_received','fieldtype': 'Select','label': 'Sent or Received', 'options': 'Sent\nReceived'},
+    			{'default': 'Open', 'fieldname': 'status', 'fieldtype': 'Select', 'label': 'Status', 'options': 'Open\nReplied\nClosed\nLinked'},
+    			{'default': cur_frm.doc.prospect_owner, 'fieldname': 'sender','fieldtype': 'Link', 'label': 'Sender', 'options': 'User', 'reqd': 1},
+  				{'fieldname': 'sectionbreak', 'fieldtype': 'Section Break'},
+  				{'fieldname': 'subject', 'fieldtype': 'Data', 'label': 'Subject'},
+  				{'fieldname': 'content', 'fieldtype': 'Small Text', 'label': 'Content'}
+  			],
+			function(data){
+				var icecube = data.recipient;
+				msgprint("Added Communication to " + icecube);
+				}
+			);
 	}
 });
 
